cc8bbaf284877e4f11a4d08124981d6da07a71ba
kajimura <k2hmr94@gmail.com>
2021-06-16 14:49:10 +0900

    routes/User.jsに投稿機能をまとめた、アカウントページ用のユーザー情報を取得

--- index.js
+++ index.js
@@ -6,30 +6,11 @@ const PORT = 3001;
 
 const db = require("./models");
 
-const { User } = require("./models");
-
 app.use(express.json());
 app.use(cors());
 
-app.post("/signin", (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const googleId = req.body.googleId;
-  const channelName = req.body.channelName;
-  const profilePictureUrl = req.body.imageUrl;
-
-  User.create({
-    name: name,
-    email: email,
-    googleId: googleId,
-    channelName: channelName,
-    profilePictureUrl: profilePictureUrl,
-  }).catch((err) => {
-    console.log(err);
-  });
-
-  res.send("inserted into database");
-});
+const userRoute = require("./routes/User");
+app.use("/user", userRoute);
 
 db.sequelize.sync().then((req) => {
   app.listen(PORT, () => {
---
+++ routes/User.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models");
+
+router.get("/:id", async(req, res) => {
+  const id = req.params.id;
+  const response = await User.findAll({
+    where: {
+      googleId: id,
+    },
+  });
+
+  res.send(response);
+});
+
+router.post("/", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const googleId = req.body.googleId;
+  const channelName = req.body.channelName;
+  const profilePictureUrl = req.body.imageUrl;
+
+  User.create({
+    name: name,
+    email: email,
+    googleId: googleId,
+    channelName: channelName,
+    profilePictureUrl: profilePictureUrl,
+  }).catch((err) => {
+    console.log(err);
+  });
+
+  res.send("inserted into database");
+});
+
+module.exports = router;
